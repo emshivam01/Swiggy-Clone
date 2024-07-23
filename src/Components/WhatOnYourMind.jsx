fe177a0aff850d8882e6489deb4594e1ebc28011
Shivam Sharma <emshivam01@gmail.com>
2024-07-23 18:58:01 +0530

    bug fix

--- src/Components/WhatOnYourMind.jsx
+++ src/Components/WhatOnYourMind.jsx
@@ -14,7 +14,7 @@ const WhatOnYourMind = () => {
   const fetchGridImage = async () => {
     try {
       const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const resData = await data.json();
       const imageGridCardsInfo =
