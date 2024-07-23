581c87d30c9b1f7589db8af0fd4798f99ede038d
Shivam Sharma <emshivam01@gmail.com>
2024-07-23 18:55:43 +0530

    cors bug fix

--- src/Components/RestaurantContainer.jsx
+++ src/Components/RestaurantContainer.jsx
@@ -10,7 +10,7 @@ const RestaurantContainer = ({ search }) => {
   const fetchRestaurant = async () => {
     try {
       const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
 
       const resData = await data.json();
